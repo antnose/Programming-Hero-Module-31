1deefe95f34e2fc5b15398ce264f302d0b27ba2f
antnose <antnose28@gmail.com>
2025-01-30 16:35:14 +0600

    Complete 2nd video of 30th module

--- module31.js
+++ module31.js
@@ -12,21 +12,74 @@
  * 8. for in loop used in object
  */
 
-const a = 56;
-const numbers = [55, 9, 8];
-const person = {
-  name: "sakib Khan",
-};
+// const a = 56;
+// const numbers = [55, 9, 8];
+// const person = {
+//   name: "sakib Khan",
+// };
 
-const message = `Hi, ${person.name} has a  ${a} access to ${numbers[2]}`;
-console.log(message);
+// const message = `Hi, ${person.name} has a  ${a} access to ${numbers[2]}`;
+// console.log(message);
 
-if (true) {
-}
+// if (true) {
+// }
 
-const square = (x) => x * x;
-const sum = (a, b) => a + b;
+// const square = (x) => x * x;
+// const sum = (a, b) => a + b;
 
-const { age, z, ...others } = { x: 2, y: 5, z: 3, name: "Ovijit", age: 55 };
+// const { age, z, ...others } = { x: 2, y: 5, z: 3, name: "Ovijit", age: 55 };
 
-const [first, second, ...remaining] = ["sam", " jodu", "modhu"];
+// const [first, second, ...remaining] = ["sam", " jodu", "modhu"];
+
+// 31-2 Access Value, Nested Object, Optional Chaining
+/**
+ * 1. Date Access
+ */
+
+// const data = [{ id: 1, name: "Abul", address: "Kochukhet" }];
+
+// console.log(data[0].address);
+
+// const products = {
+//   count: 5000,
+//   data: [
+//     {
+//       id: 1,
+//       name: "Lenevo",
+//       price: 65000,
+//     },
+//     {
+//       id: 2,
+//       name: "macbook",
+//       price: 16500,
+//     },
+//   ],
+// };
+
+// console.log(products.data[1].id);
+
+// const user = {
+//   id: 5001,
+//   name: "Shoriful Raj",
+//   address: {
+//     street: {
+//       first: "52/1 uttor side",
+//       second: "Poribag er goli",
+//       third: "Nodora",
+//     },
+//     city: "Dhaka",
+//   },
+// };
+// console.log(user.address.street.second);
+
+// const user2 = {
+//   id: 44403,
+//   name: "Pori Bibir Majar",
+//   address: {
+//     city: "Dhaka",
+//     country: "BD",
+//   },
+// };
+
+// console.log(user2.address.street?.second);
+// I use a question mark after street this question mark called optional chain
